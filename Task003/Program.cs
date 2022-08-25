8f35157fde6b77706778c922ac39c53461df4b28
«Kristina» <kristinabol1996@gmail.com>
2022-08-26 00:34:01 +0800

    3 задача, перевод из десятичной в двоичную систему

---
+++ Task003/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+int GetLengthArray(int number)
+{
+    int len = 0;
+    while (number >= 1)
+    {
+        number = number / 2;
+        len++;
+    }
+    return len;
+}
+
+int[] ConvertToBinaryNumber(int number)
+{
+    int[] array = new int[GetLengthArray(number)];
+    for (int i = array.Length - 1; i >= 0; i--)
+    {
+        array[i] = number % 2;
+        number = number / 2;
+    }
+    return array;
+}
+
+void PrintConvertToBinaryNumber(int[] array)
+{
+    for (int i = 0; i < array.Length; i++) Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+
+int[] arr = ConvertToBinaryNumber(47);
+PrintConvertToBinaryNumber(arr);
